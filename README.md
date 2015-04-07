f4b7d7857a63b133485d86866b1f7ed9b00f2c0b
Peter Edge <peter.edge@gmail.com>
2015-04-07 17:01:44 +0200

    commit

--- README.md
+++ README.md
@@ -13,3 +13,83 @@ import (
 )
 ```
 
+
+#### func  DockerBinds
+
+```go
+func DockerBinds(hostVolumeToVolume map[string]string) []string
+```
+
+#### func  DockerExposedPorts
+
+```go
+func DockerExposedPorts(expose []uint16) map[docker.Port]struct{}
+```
+
+#### func  DockerVolumes
+
+```go
+func DockerVolumes(volumes []string) map[string]struct{}
+```
+
+#### func  DockerVolumesFrom
+
+```go
+func DockerVolumesFrom(volumesFrom []string) string
+```
+
+#### func  IsDockerTls
+
+```go
+func IsDockerTls(dockerTlsVerify string) bool
+```
+
+#### func  NewDockerClient
+
+```go
+func NewDockerClient(dockerHost string, dockerTlsVerify bool, dockerCertPath string, apiVersion string) (*docker.Client, error)
+```
+
+#### func  NewDockerClientFromEnv
+
+```go
+func NewDockerClientFromEnv(apiVersion string) (*docker.Client, error)
+```
+
+#### type DockerEnvironment
+
+```go
+type DockerEnvironment struct {
+	DockerHost      string
+	DockerTlsVerify string
+	DockerCertPath  string
+}
+```
+
+
+#### func  GetDockerEnvironment
+
+```go
+func GetDockerEnvironment() (*DockerEnvironment, error)
+```
+TODO(pedge): we are assuming the DOCKER_CERT_PATH is a directory within the host
+regardless of if we are running in a docker container or not, this should
+actually still work but this is not great
+
+#### func (*DockerEnvironment) EnvStrings
+
+```go
+func (this *DockerEnvironment) EnvStrings() []string
+```
+
+#### func (*DockerEnvironment) HostVolumeToVolume
+
+```go
+func (this *DockerEnvironment) HostVolumeToVolume() map[string]string
+```
+
+#### func (*DockerEnvironment) IsDockerTls
+
+```go
+func (this *DockerEnvironment) IsDockerTls() bool
+```
--- dockerutils.go
+++ dockerutils.go
@@ -1,1 +1,187 @@
 package dockerutils
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/fsouza/go-dockerclient"
+)
+
+type DockerEnvironment struct {
+	DockerHost      string
+	DockerTlsVerify string
+	DockerCertPath  string
+}
+
+func (this *DockerEnvironment) IsDockerTls() bool {
+	return IsDockerTls(this.DockerTlsVerify)
+}
+
+func (this *DockerEnvironment) EnvStrings() []string {
+	return []string{
+		fmt.Sprintf("DOCKER_HOST=%s", this.DockerHost),
+		fmt.Sprintf("DOCKER_TLS_VERIFY=%s", this.DockerTlsVerify),
+		fmt.Sprintf("DOCKER_CERT_PATH=%s", this.DockerCertPath),
+	}
+}
+
+func (this *DockerEnvironment) HostVolumeToVolume() map[string]string {
+	if !this.IsDockerTls() || this.DockerCertPath == "" {
+		return nil
+	}
+	return map[string]string{
+		this.DockerCertPath: this.DockerCertPath,
+	}
+}
+
+// TODO(pedge): we are assuming the DOCKER_CERT_PATH is a directory within the host
+// regardless of if we are running in a docker container or not, this should actually
+// still work but this is not great
+func GetDockerEnvironment() (*DockerEnvironment, error) {
+	dockerHost := os.Getenv("DOCKER_HOST")
+	if dockerHost == "" {
+		return nil, errors.New("dockerutils: environment variable DOCKER_HOST must be set")
+	}
+	dockerTlsVerify := os.Getenv("DOCKER_TLS_VERIFY")
+	var dockerCertPath string
+	var err error
+	if IsDockerTls(dockerTlsVerify) {
+		dockerCertPath = os.Getenv("DOCKER_CERT_PATH")
+		if dockerCertPath == "" {
+			home := os.Getenv("HOME")
+			if home == "" {
+				return nil, errors.New("dockerutils: environment variable HOME must be set if DOCKER_CERT_PATH is not set")
+			}
+			dockerCertPath = filepath.Join(home, ".docker")
+			dockerCertPath, err = filepath.Abs(dockerCertPath)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return &DockerEnvironment{
+		DockerHost:      dockerHost,
+		DockerTlsVerify: dockerTlsVerify,
+		DockerCertPath:  dockerCertPath,
+	}, nil
+}
+
+func NewDockerClientFromEnv(apiVersion string) (*docker.Client, error) {
+	dockerEnvironment, err := GetDockerEnvironment()
+	if err != nil {
+		return nil, err
+	}
+	dockerHost := dockerEnvironment.DockerHost
+	if dockerEnvironment.IsDockerTls() {
+		parts := strings.SplitN(dockerHost, "://", 2)
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("dockerutils: could not split %s into two parts by ://", dockerHost)
+		}
+		dockerHost = fmt.Sprintf("https://%s", parts[1])
+		dockerCertPath := dockerEnvironment.DockerCertPath
+		if err := checkFileExists(dockerCertPath); err != nil {
+			return nil, err
+		}
+		cert := filepath.Join(dockerCertPath, "cert.pem")
+		if err := checkFileExists(cert); err != nil {
+			return nil, err
+		}
+		key := filepath.Join(dockerCertPath, "key.pem")
+		if err := checkFileExists(key); err != nil {
+			return nil, err
+		}
+		ca := filepath.Join(dockerCertPath, "ca.pem")
+		if err := checkFileExists(ca); err != nil {
+			return nil, err
+		}
+		return NewDockerClient(dockerHost, true, dockerCertPath, apiVersion)
+	}
+	return NewDockerClient(dockerEnvironment.DockerHost, false, "", apiVersion)
+}
+
+func NewDockerClient(dockerHost string, dockerTlsVerify bool, dockerCertPath string, apiVersion string) (*docker.Client, error) {
+	if dockerTlsVerify {
+		cert := filepath.Join(dockerCertPath, "cert.pem")
+		key := filepath.Join(dockerCertPath, "key.pem")
+		ca := filepath.Join(dockerCertPath, "ca.pem")
+		return docker.NewVersionedTLSClient(dockerHost, cert, key, ca, apiVersion)
+	}
+	return docker.NewVersionedClient(dockerHost, apiVersion)
+}
+
+func IsDockerTls(dockerTlsVerify string) bool {
+	// TODO(pedge): is any non-empty value legal? any non-zero value?
+	return dockerTlsVerify == "1"
+}
+
+func DockerExposedPorts(expose []uint16) map[docker.Port]struct{} {
+	if expose == nil || len(expose) == 0 {
+		return nil
+	}
+	m := make(map[docker.Port]struct{})
+	for _, port := range expose {
+		m[docker.Port(fmt.Sprintf("%v/tcp", port))] = emptyStruct()
+	}
+	return m
+}
+
+func DockerVolumes(volumes []string) map[string]struct{} {
+	if volumes == nil || len(volumes) == 0 {
+		return nil
+	}
+	m := make(map[string]struct{})
+	for _, volume := range volumes {
+		m[volume] = emptyStruct()
+	}
+	return m
+}
+
+func DockerVolumesFrom(volumesFrom []string) string {
+	return strings.Join(volumesFrom, ",")
+}
+
+func DockerBinds(hostVolumeToVolume map[string]string) []string {
+	if hostVolumeToVolume == nil || len(hostVolumeToVolume) == 0 {
+		return nil
+	}
+	binds := make([]string, len(hostVolumeToVolume))
+	i := 0
+	for hostVolume, volume := range hostVolumeToVolume {
+		binds[i] = fmt.Sprintf("%s:%s:rw", hostVolume, volume)
+		i++
+	}
+	return binds
+}
+
+// ***** PRIVATE *****
+
+func checkFileExists(path string) error {
+	exists, err := isFileExists(path)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return fmt.Errorf("docker: file %s does not exist", path)
+	}
+	return nil
+}
+
+func isFileExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+// TODO(pedge): what?
+func emptyStruct() struct{} {
+	var str struct{}
+	return str
+}
